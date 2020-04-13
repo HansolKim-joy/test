e25268767c0eb9d2e0b1aa17e8177e7ab2527680
HansolKim-joy <goodkhs1234@naver.com>
2020-04-13 16:38:28 +0900

    일단 오류난 부분 잡앗어요!

--- Watch_Next/src/member/model/dao/MemberDAO.java
+++ Watch_Next/src/member/model/dao/MemberDAO.java
@@ -50,10 +50,12 @@ public class MemberDAO {
 				String userName = rset.getString("USER_NAME");
 				String phone = rset.getString("USER_PHONE");
 				String email = rset.getString("USER_EMAIL");
+				String mailingYN = rset.getString("mailing");
+				String adminYN = rset.getString("admin_Yn");
 				String deleteYN = rset.getString("USER_DELETE");
 				
 				loginUser = new Member(userId, userPwd, userName, phone, email, mailingYN, adminYN, deleteYN);
-			
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
