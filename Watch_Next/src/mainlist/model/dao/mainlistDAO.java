4742dd44dbe20907df83630148019fc822d4967e
OwenSuh <jeongsuh4613@gmail.com>
2020-04-17 21:48:02 +0900

    건들지 마시오,

--- Watch_Next/src/mainlist/model/dao/mainlistDAO.java
+++ Watch_Next/src/mainlist/model/dao/mainlistDAO.java
@@ -1,5 +1,14 @@
 package mainlist.model.dao;
 
+import java.util.ArrayList;
+
+import recruit.model.vo.Recruit;
+
 public class mainlistDAO {
+
+	public ArrayList<Recruit> selectRelist() {
+		
+		return null;
+	}
 
 }
